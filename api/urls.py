49907a684f661bb5d7ef7e73944a099bcb8a3a93
David Kahara <kaharadave@gmail.com>
2021-05-08 10:25:51 +0300

    Custom Cocktail endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import CocktailDetailView, RandomCocktailView
+from .views import CocktailDetailView,CustomCocktailCreateView, RandomCocktailView
 
 app_name = "api"
 
 
 urlpatterns = [
     path("cocktails/random/", RandomCocktailView.as_view(), name='random-cocktails'),
-    path("cocktails/<int:drink_id>/", CocktailDetailView.as_view(), name='cocktails')
+    path("cocktails/<int:drink_id>/", CocktailDetailView.as_view(), name='cocktails'),
+    path("cocktails/", CustomCocktailCreateView.as_view(), name='create-custom-cocktail')
     ]
--- api/views.py
+++ api/views.py
@@ -2,13 +2,13 @@ import requests
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.generics import RetrieveUpdateDestroyAPIView
+from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
 
 from .serializers import CocktailSerializer
 from .models import Cocktail
 
 class RandomCocktailView(APIView):
-    """This endpoint returns a list of five random cocktails fetched from theCocktailDb API
+    """This endpoint returns a list of five random cocktails fetched from TheCocktailDB API
     """
 
     def get(self, request, *args, **kwargs):
@@ -24,7 +24,16 @@ class RandomCocktailView(APIView):
 
     
 class CocktailDetailView(RetrieveUpdateDestroyAPIView):
+    """This endpoint retrieves, updates or delets a single Cocktail's details given the id
+    """
 
     serializer_class = CocktailSerializer
     lookup_url_kwarg = 'drink_id'
     queryset = Cocktail.objects.all()
+
+class CustomCocktailCreateView(CreateAPIView):
+    def post(self, request):
+            serializer = CocktailSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
