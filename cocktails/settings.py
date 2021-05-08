d5134b9907607ecffebd4ec2661b83c289632293
David Kahara <kaharadave@gmail.com>
2021-05-08 14:31:10 +0300

    Configure CI with github actions

--- cocktails/settings.py
+++ cocktails/settings.py
@@ -88,6 +88,17 @@ DATABASES = {
     }
 }
 
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME': 'github_actions',
+           'USER': 'postgres',
+           'PASSWORD': 'postgres',
+           'HOST': '127.0.0.1',
+           'PORT': '5432',
+        }
+    }
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
 db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=True)
